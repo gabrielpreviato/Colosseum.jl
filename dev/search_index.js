ba6e8a6c7bf9eb6963a377158da98d9076fba6eb
Documenter.jl <documenter@juliadocs.github.io>
2023-02-28 22:26:50 +0000

    build based on d407b6f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Colosseum","category":"page"},{"location":"#Colosseum","page":"Home","title":"Colosseum","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Colosseum.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Colosseum]","category":"page"}]
+}
